242e88ead6c923308679d21886c949bc86b3ad77
KianaKun <hilmananz@gmail.com>
2024-06-11 21:28:48 +0700

    creating information to patient that are accepted or denied(still process)

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+// app.js
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -5,38 +7,41 @@ const connection = require('./utils/database');
 const app = express();
 const port = 3000;
 
-app.set('view engine','ejs');
-app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
-    extended:true
-}))
+    extended: true
+}));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/appointment',(req,res)=>{
-    res.render('form')
-})
+app.get('/appointment', (req, res) => {
+    res.render('form');
+});
 
-app.post('/submit',(req,res)=>{
-    const {name, gender, phone, birthdate, address, email, complain, meetingDate}=req.body;
-    const query='INSERT INTO form (name, gender, phone, birthdate, address, email, complain, meetingDate) VALUES(?,?,?,?,?,?,?,?)';
-    connection.query(query,[name, gender, phone, birthdate, address, email, complain, meetingDate],(err,result)=>{
-        if(err){
-            console.error(`Error Mengisi Data:${err}`);
+app.post('/appointment/submit', (req, res) => {
+    const { name, gender, phone, birthdate, address, email, complain, meetingDate } = req.body;
+    const query = 'INSERT INTO form (name, gender, phone, birthdate, address, email, complain, meetingDate) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    connection.query(query, [name, gender, phone, birthdate, address, email, complain, meetingDate], (err, result) => {
+        if (err) {
+            console.error(`Error Mengisi Data: ${err}`);
             return res.status(500).send('Internal Server Error');
         }
-        res.send('Berhasil Disimpan!');
-    })
-})
+        console.log('Data berhasil di input');
+        res.redirect('/information');
+    });
+});
 
-app.get('/login',(req,res)=>{
-    res.render('login')
-})
+app.get('/login', (req, res) => {
+    res.render('login');
+});
 
+app.get('/information', (req, res) => {
+    res.render('information');
+});
 
-
-app.listen(port,(req,res)=>{
+app.listen(port, () => {
     console.log(`Server berhasil dijalankan di http://localhost:${port}`);
 });
